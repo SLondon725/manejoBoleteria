ae1327567e0e76e76ade22e8abe2942650a01a63
SLONDON725 <decriloba204@gmail.com>
2025-10-21 19:56:52 -0500

    Enviando peticion de registro de eventos

--- Frontend/js/modulos/eventos.js
+++ Frontend/js/modulos/eventos.js
@@ -1,22 +1,36 @@
 let formulario = document.getElementById("formulario_evento");
-let nombreEvento = formulario.getElementById("nombre_evento").value;
-let descripcionEvento = document.getElementById("descripcion_evento").value;
-let fechaInicioEvento = document.getElementById("fecha_evento").value;
-let horaInicioEvento = document.getElementById("horaI_evento").value;
-let horaFinEvento = document.getElementById("horaF_evento").value;
+let nombreEvento = document.getElementById("nombre_evento");
+let descripcionEvento = document.getElementById("descripcion_evento");
+let fechaInicioEvento = document.getElementById("fecha_evento");
+let horaInicioEvento = document.getElementById("horaI_evento");
+let horaFinEvento = document.getElementById("horaF_evento");
 
 
 formulario.addEventListener("submit", (e) => {
 
     e.preventDefault();
     let evento = {
-        nombre: nombreEvento,
-        descripcion: descripcionEvento,
-        fechaI: fechaInicioEvento,
-        horaI: horaInicioEvento,
-        horaF: horaFinEvento
+        nombre: nombreEvento.value,
+        descripcion: descripcionEvento.value,
+        fechaI: fechaInicioEvento.value,
+        horaI: horaInicioEvento.value,
+        horaF: horaFinEvento.value
     }
 
+    registrar(evento);
     console.log(evento)
 });
+
+const registrar = async (evento) =>{
+
+    const res = await fetch("http://localhost:8080/eventos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(evento)
+    }) 
+
+    return res.json();
+}
 
